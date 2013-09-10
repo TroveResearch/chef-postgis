5211b86509a21d5d5873891d26c4af2ebb3de2e4
Victor Miroshnikov <me@vmiroshnikov.com>
2013-09-10 04:50:06 +0200

    Grant priveleges on spatial stuff

--- recipes/_init_database_template.rb
+++ recipes/_init_database_template.rb
@@ -4,7 +4,10 @@ execute 'create_postgis_template' do
   command <<CMD
 (createdb -E UTF8 --locale=#{node['postgis']['locale']} #{node['postgis']['template_name']} -T template0) &&
 (psql -d #{node['postgis']['template_name']} -f `pg_config --sharedir`/contrib/postgis-2.0/postgis.sql) &&
-(psql -d #{node['postgis']['template_name']} -f `pg_config --sharedir`/contrib/postgis-2.0/spatial_ref_sys.sql)
+(psql -d #{node['postgis']['template_name']} -f `pg_config --sharedir`/contrib/postgis-2.0/spatial_ref_sys.sql) &&
+(psql -d #{node['postgis']['template_name']} -c "GRANT ALL ON geometry_columns TO PUBLIC;") &&
+(psql -d #{node['postgis']['template_name']} -c "GRANT ALL ON geography_columns TO PUBLIC;") &&
+(psql -d #{node['postgis']['template_name']} -c "GRANT ALL ON spatial_ref_sys TO PUBLIC;") 
 CMD
   action :run
 end
